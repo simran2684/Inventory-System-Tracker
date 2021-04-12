a413c32af382b7620e53cb907f64df418f45fd06
hafsazia <47464997+hafsazia@users.noreply.github.com>
2021-04-11 22:23:33 -0600

    Create getDependents.php

---
+++ api/Dependents/getDependents.php
@@ -0,0 +1,48 @@
+<?php
+
+// Headers 
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Dependents.php';
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate dependents object
+$dependents = new Dependents($db);
+
+// Product query
+$result = $dependents->read();        // result from the read function
+// Get row count
+$num = $result->rowCount();
+
+// Check if any dependents
+if($num > 0){
+    // Dependents array
+    $dep_arr = array();
+    $dep_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $dep_item = array(
+            'EmployeeID' => $employeeid,
+            'Name' => $name,
+            'PhoneNumber' => $phoneNum
+           );
+
+        // Push to "data"
+        array_push($dep_arr['data'], $dep_item);
+    }
+
+    // Turn to JSON & output 
+    echo json_encode($dep_arr);
+} else{
+    // No dependents
+    echo json_encode(
+        array('message' => 'No Dependents Found')
+    );
+}
