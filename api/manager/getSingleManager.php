707fb86fac9523d783a87748362047cbf0552d81
hafsazia <47464997+hafsazia@users.noreply.github.com>
2021-04-12 22:03:21 -0600

    Create getSingleManager.php

---
+++ api/manager/getSingleManager.php
@@ -0,0 +1,31 @@
+<?php
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Manager.php';
+    
+    //Connect the Database
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate manager object
+    $manager = new Manager($db);
+
+    // Get ssn
+    $manager->mgrSSN = isset($_GET['mgrSSN']) ? $_GET['mgrSSN'] : die();
+
+    // Call getSingleManager method
+    $manager->getSingleManager();
+
+    // Create array
+    $manager_arr = array(
+        'MgrSSN' => $manager->mgrSSN,
+        'ID' => $manager->ID,
+        'StoreLocation' => $manager->storeLocation
+        
+    );
+
+    // Make JSON
+    print_r(json_encode($manager_arr));
