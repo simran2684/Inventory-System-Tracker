daaf7e0611591a2c1c5148aa317f36c1b7da6035
hafsazia <47464997+hafsazia@users.noreply.github.com>
2021-04-12 22:00:11 -0600

    Create createManager.php

---
+++ api/manager/createManager.php
@@ -0,0 +1,35 @@
+<?php
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Manager.php';
+    
+    //Connect the Database
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate Manager object
+    $manager = new Manager($db);
+
+    // Get the raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    $manager->mgrSSN = $data->mgrSSN;
+    $manager->ID = $data->ID;
+    $manager->storeLocation = $data->storeLocation;
+   
+
+    // Create manager
+    if ($manager->createManager()) {
+        echo json_encode(
+            array('message' => 'Manager Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Manager Not Created')
+        );
+    }
