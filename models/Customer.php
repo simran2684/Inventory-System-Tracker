15d16dfb297c71ec79a1e8979d3effa73184be76
hafsazia <47464997+hafsazia@users.noreply.github.com>
2021-04-12 18:00:07 -0600

    Create Customer.php

---
+++ models/Customer.php
@@ -0,0 +1,34 @@
+<?php
+    class Customer{
+        private $connect;
+        private $table = 'customer';
+
+        //customer properties
+        public $customerNum;
+        public $paymentMethod;
+        
+        //DB constructor
+        public function _constructor($db) {
+            $this -> connect = $db;
+        }
+
+        //Get Customer
+        public function read(){
+            //Create query
+            $query = 'Select
+                c.customerNum,
+                d.paymentMethod,
+                
+            FROM
+                ' . $this->table . ' c';
+
+            //preparing statement
+            $stmt = $this->connect->prepare($query);
+
+            //Execute the query
+            $stmt->execute();
+
+            return $stmt; 
+        }
+
+    }
