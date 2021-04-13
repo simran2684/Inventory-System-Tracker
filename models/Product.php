77597e290ff6c9929c2fccece0657e1480ff9472
simran2684 <47336181+simran2684@users.noreply.github.com>
2021-04-12 18:19:04 -0600

    Add files via upload

--- models/Product.php
+++ models/Product.php
@@ -1,6 +1,6 @@
 <?php
-    class Post{
-        private $conn;
+    class Product{
+        private $connect;
         private $table = 'product';
 
         // Post Properties
@@ -16,7 +16,7 @@
 
         // Constructor with DB
         public function __construct($db){
-            $this->conn = $db;
+            $this->connect = $db;
         }
 
         // Get Posts
@@ -35,12 +35,53 @@
             ' . $this->table . ' p';
 
             //Prepare statement
-            $stmt = $this->conn->prepare($query);
+            $stmt = $this->connect->prepare($query);
 
             // Execute query
             $stmt->execute();
 
             return $stmt;
+
+        }
+
+
+        // Get a single employee
+        public function getSingleProduct() {
+            $query = 'SELECT p.productNum, 
+            p.name,
+            p.brand,
+            p.category, 
+            p.quantity,
+            p.weight,
+            p.inventoryNum,
+            p.location,
+            p.storageTemp
+            FROM
+            ' . $this->table . ' p
+            WHERE
+                p.productNum = ?
+            LIMIT 0,1';
+
+            // Prepare statement
+            $stmt = $this->connect->prepare($query);
+
+            // Bind id
+            $stmt->bindParam(1, $this->productNum);
+
+            // Execute Query
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // Set properties
+            $this->name = $row['name'];
+            $this->brand = $row['brand'];
+            $this->category = $row['category'];
+            $this->quantity = $row['quantity'];
+            $this->weight = $row['weight'];
+            $this->inventoryNum = $row['inventoryNum'];
+            $this->location=$row['location'];
+            $this->storageTemp=$row['storageTemp'];
 
         }
 
