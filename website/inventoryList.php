ae1b2ea54eaa8cee3df92764c3b9f72a965b0c3a
hafsazia <47464997+hafsazia@users.noreply.github.com>
2021-04-15 01:57:45 -0600

    Add files via upload

---
+++ website/inventoryList.php
@@ -0,0 +1,70 @@
+<?php
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Content-Type: text/html');
+
+  include_once '../config/Database.php';
+  include_once '../models/Product.php';
+
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate inventory object
+  $inventory = new Product($db);
+
+  // Get inventory query
+  $result = $inventory->getProducts();
+
+ // $row = $result->fetch(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <h1>Inventory</h1>
+</head>
+
+<body>
+  <div>
+    <button class = "backButton" onclick="window.location.href = 'index.html'">Back</button>
+  </div>
+
+  <div id = 'table'>
+    <table class="inventoryTable">
+      <thead>
+        <tr>
+          <th>Product Number</th>
+          <th>Name</th>
+          <th>Brand</th>
+          <th>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+          extract($row);
+        
+      ?>
+        <tr>
+          <td class="attribute"><?php echo $row['productNum'];?></td>
+          <td class="attribute"><?php echo $row['name']?></td>
+          <td class="attribute"><?php echo $row['brand']?></td>
+          <td class="attribute" contenteditable = "true"><?php echo $row['quantity']?></td>
+          <td> 
+            <div>
+               <button class="btns2" onclick="window.location.href = 'inventoryList.php'">Update Quantity</button>
+            </div>
+          </td>
+        </tr>
+        <?php } ?>
+        
+      </tbody>
+      
+    </table>
+    
+  </div>
+</body>
+
+
+</html>
