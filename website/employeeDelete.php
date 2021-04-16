bf4d3f2342453cbd01f86ce1cf68c3b195534bd3
Joshua <48004192+Splaysational@users.noreply.github.com>
2021-04-15 23:25:59 -0600

    Add files via upload

--- website/employeeDelete.php
+++ website/employeeDelete.php
@@ -7,6 +7,11 @@
 
     include_once '../config/Database.php';
     include_once '../models/Employee.php';
+    include_once '../models/Clerk.php';
+    include_once '../models/Administrator.php';
+    include_once '../models/Manager.php';
+    include_once '../models/PhoneNumber.php';
+    include_once '../models/Dependents.php';
     
     //Connect the Database
     $database = new Database();
@@ -14,6 +19,37 @@
 
     // Instantiate employee object
     $employee = new Employee($db);
+
+    $employee->status = $_GET['position'];
+
+    // Below is the check to delete position, but if we're deleting only clerks, we might not even need the check
+    // and we can just delete the clerk from the table.
+    if ($employee->status == "Clerk") {
+        $clerk = new Clerk($db);
+        $clerk->id = $employee->employeeId;
+        $clerk->deleteClerk();
+    }
+    // } else if ($employee->status == "Manager") {
+    //     $manager = new Manager($db);
+    //     $manager->id = $employee->employeeId;
+    //     $manager->deleteManager();
+    // } else if ($employee->status = "Admin") {
+    //     $admin = new Administrator($db);
+    //     $admin->id = $employee->employeeId;
+    //     $admin->deleteManager();
+    // }
+    
+    //Get the corresponding dependent, and then delete it.
+    $dependent= new Dependent($db);
+    $dependent->employeeid = $employee->employeeId;
+    $dependent->deleteDependent();
+
+    // Get the corresponding phonenumber, and then delete it.
+    $p_num = new PhoneNumber($db);
+    $p_num->employeeId = $employee->employeeId;
+    $p_num->deleteNumber();
+
+    //Hopefully once these all delete nicely, then we can delete the employee without any trouble.
 
     // Set the id for the delete
     $employee->employeeId = $_GET['employeeId'];
