0b28e4c4ed4cf2bc1fac783a329cedaf29f83a4c
simran2684 <47336181+simran2684@users.noreply.github.com>
2021-06-24 19:20:52 -0600

    update

--- website/productMain.php
+++ website/productMain.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>repl.it</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
 
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width">
-	<title>repl.it</title>
-	<link href="style.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-  <div>
-    <button class = "backButton" onclick="window.location.href = 'index.html'">Back</button>
-  </div>
-	<script src="script.js">
-	</script>
-	<h1>Product</h1>
-	<div>
-		<button class="btns" onclick="window.location.href = 'viewProducts.php'">View Products</button>
+  <body>
+    <div>
+      <button class="backButton" onclick="window.location.href = 'index.html'">
+        Back
+      </button>
+    </div>
+    <script src="script.js"></script>
+    <h1>Products</h1>
+    <div>
+      <button class="btns" onclick="window.location.href = 'viewProducts.php'">
+        View Products
+      </button>
     </div>
     <div>
-      <button class="btns" onclick="window.location.href = 'createProduct.php'">Add Products</button>
+      <button class="btns" onclick="window.location.href = 'createProduct.php'">
+        Add Products
+      </button>
     </div>
   </body>
 </html>
